036e093719af4974f9d3872d1a7cce26eb012783
ROHITH <pythonchannel250104@gmail.com>
2025-02-21 18:07:16 +0530

    Update CLASSIFICATION.py

--- CLASSIFICATION.py
+++ CLASSIFICATION.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 
 class Classification:
     def __init__(self, dataset):
@@ -11,20 +12,12 @@ class Classification:
         # Content for Perform Operations tab
         with tab1:
             # Create two columns with a ratio of 1:2
-            col1, col2 = st.columns([1, 2])
-
-            # Add a radio button in the first column
-            with col1:
-                operation = st.radio("Select Operation", ["Train Test Split", "Classifiers"])
-
-            # You can add more content to col2 as needed
-            with col2:
-                if operation == "Train Test Split":
-                    st.write("You selected Train Test Split")
-                    # Add more content related to Train Test Split
-                elif operation == "Classifiers":
-                    st.write("You selected Classifiers")
-                    # Add more content related to Classifiers
+            col1, col2 = st.columns([1, 2],border=True)
+            operation = col1.radio("Select Operation", ["Train Test Split", "Classifiers"])
+            if operation == "Train Test Split":
+                col2.write("You selected Train Test Split")
+            elif operation == "Classifiers":
+                col2.write("You selected Classifiers")
 
         # Content for View Operations tab
         with tab2:
