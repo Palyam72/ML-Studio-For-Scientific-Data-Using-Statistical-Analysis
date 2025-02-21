94d4835af12cf40bbefb116def9e7052185be025
ROHITH <pythonchannel250104@gmail.com>
2025-02-22 01:21:03 +0530

    Update CLASSIFICATION.py

--- CLASSIFICATION.py
+++ CLASSIFICATION.py
@@ -82,6 +82,8 @@ class Classification:
                 self.one_class_svm_classifier(col2)
               elif option == "SVC":
                 self.svc_classifier(col2)
+              elif option == "KNeighbours Classifier":
+                self.knn_classifier(col2)
                 
       with tab2:
           col1, col2 = st.columns([1, 2], border=True)
@@ -718,6 +720,47 @@ class Classification:
         col2.success("Model Fitted Successfully")
         col2.divider()
         self.metrics(col2, st.session_state["SVC"])
-      
-          
-                  
+  
+    def knn_classifier(self, col2):
+      xtrain_key = col2.selectbox("Select X Train for KNN", list(st.session_state["availableDatasets"].keys()))
+      ytrain_key = col2.selectbox("Select Y Train for KNN", list(st.session_state["availableDatasets"].keys()))
+  
+      # Model parameters selection
+      n_neighbors = col2.number_input("Number of Neighbors (k)", value=5, min_value=1, step=1)
+      weights = col2.selectbox("Weight Function", ["uniform", "distance"], index=0)
+      algorithm = col2.selectbox("Algorithm", ["auto", "ball_tree", "kd_tree", "brute"], index=0)
+      leaf_size = col2.number_input("Leaf Size", value=30, min_value=1, step=1)
+      p = col2.number_input("Power Parameter for Minkowski Distance", value=2, min_value=1, step=1)
+      metric = col2.selectbox("Distance Metric", ["minkowski", "euclidean", "manhattan", "chebyshev"], index=0)
+      n_jobs = col2.number_input("Number of Jobs (Parallel Processing)", value=None, step=1, format="%d")
+  
+      col2.divider()
+  
+      if col2.checkbox("Continue To Fit The Model"):
+          model = KNeighborsClassifier(
+              n_neighbors=n_neighbors,
+              weights=weights,
+              algorithm=algorithm,
+              leaf_size=leaf_size,
+              p=p,
+              metric=metric,
+              n_jobs=n_jobs if n_jobs else None
+          )
+  
+          col2.subheader("Your Model", divider='blue')
+          col2.write(model.get_params())
+  
+          if st.session_state.get("KNN") is None:
+              st.session_state["KNN"] = model.fit(
+                  st.session_state["availableDatasets"][xtrain_key],
+                  st.session_state["availableDatasets"][ytrain_key]
+              )
+          else:
+              col2.success("Model Created")
+              delete = col2.checkbox("Do you want to recreate the model?")
+              if delete:
+                  st.session_state["KNN"] = None
+  
+          col2.success("Model Fitted Successfully")
+          col2.divider()
+          self.metrics(col2, st.session_state["KNN"])              
