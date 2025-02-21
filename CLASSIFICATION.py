66c3c625ff3f978ae33f2bf2b2e82e46e709a7cc
ROHITH <pythonchannel250104@gmail.com>
2025-02-22 01:27:15 +0530

    Update CLASSIFICATION.py

--- CLASSIFICATION.py
+++ CLASSIFICATION.py
@@ -15,7 +15,7 @@ from sklearn.linear_model import LogisticRegression
 
 session_models=["Hist Gradient Boosting Classifier","Random Forest Classifier",
                 "Stacking Classifier","Voting Classifier","LinearSVC","NuSVC",
-               "OneClassSVM","KNN"]
+               "OneClassSVM","KNN","RadiusNeighbors"]
 for i in session_models:
     if i not in st.session_state:
         st.session_state[i]=None
@@ -84,7 +84,8 @@ class Classification:
                 self.svc_classifier(col2)
               elif option == "KNeighbours Classifier":
                 self.knn_classifier(col2)
-                
+              elif option == "Radius Neighbours Classifier":
+                self.radius_neighbors_classifier(col2)
       with tab2:
           col1, col2 = st.columns([1, 2], border=True)
           col1.subheader("Select The View Mode", divider='blue')
@@ -763,4 +764,49 @@ class Classification:
   
           col2.success("Model Fitted Successfully")
           col2.divider()
-          self.metrics(col2, st.session_state["KNN"])              
+          self.metrics(col2, st.session_state["KNN"])       
+    def radius_neighbors_classifier(self, col2):
+      xtrain_key = col2.selectbox("Select X Train for Radius Neighbors", list(st.session_state["availableDatasets"].keys()))
+      ytrain_key = col2.selectbox("Select Y Train for Radius Neighbors", list(st.session_state["availableDatasets"].keys()))
+  
+      # Model parameters selection
+      radius = col2.number_input("Radius for Neighbors", value=1.0, min_value=0.1, step=0.1)
+      weights = col2.selectbox("Weight Function", ["uniform", "distance"], index=0)
+      algorithm = col2.selectbox("Algorithm", ["auto", "ball_tree", "kd_tree", "brute"], index=0)
+      leaf_size = col2.number_input("Leaf Size", value=30, min_value=1, step=1)
+      p = col2.number_input("Power Parameter for Minkowski Distance", value=2, min_value=1, step=1)
+      metric = col2.selectbox("Distance Metric", ["minkowski", "euclidean", "manhattan", "chebyshev"], index=0)
+      outlier_label = col2.text_input("Outlier Label (leave blank for None)")
+      n_jobs = col2.number_input("Number of Jobs (Parallel Processing)", value=None, step=1, format="%d")
+  
+      col2.divider()
+  
+      if col2.checkbox("Continue To Fit The Model"):
+          model = RadiusNeighborsClassifier(
+              radius=radius,
+              weights=weights,
+              algorithm=algorithm,
+              leaf_size=leaf_size,
+              p=p,
+              metric=metric,
+              outlier_label=outlier_label if outlier_label else None,
+              n_jobs=n_jobs if n_jobs else None
+          )
+  
+          col2.subheader("Your Model", divider='blue')
+          col2.write(model.get_params())
+  
+          if st.session_state.get("RadiusNeighbors") is None:
+              st.session_state["RadiusNeighbors"] = model.fit(
+                  st.session_state["availableDatasets"][xtrain_key],
+                  st.session_state["availableDatasets"][ytrain_key]
+              )
+          else:
+              col2.success("Model Created")
+              delete = col2.checkbox("Do you want to recreate the model?")
+              if delete:
+                  st.session_state["RadiusNeighbors"] = None
+  
+          col2.success("Model Fitted Successfully")
+          col2.divider()
+          self.metrics(col2, st.session_state["RadiusNeighbors"])
