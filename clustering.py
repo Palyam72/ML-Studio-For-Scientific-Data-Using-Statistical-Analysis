5d05ee0d0576b04712a5d45d88a3da64e32edf9d
ROHITH <pythonchannel250104@gmail.com>
2025-03-12 19:52:11 +0530

    Update clustering.py

--- clustering.py
+++ clustering.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-from sklearn.clusters import *
+from sklearn.cluster import *
 
 class clusters:
   def __init__(self,df):
